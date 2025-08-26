f375bc0bc0ac25905d60ae9c31b02cdd28f1ee49
Kawanwa <kawanwa.stone@gmail.com>
2025-08-26 13:43:31 +0700

    config.js

--- config.js
+++ config.js
@@ -9,7 +9,8 @@ export const firebaseConfig = {
     apiKey: "AIzaSyC0FFHNoma6TkX5GbKdki6B5h2peYOsUu0",
     authDomain: "kawanwa-stone.firebaseapp.com",
     projectId: "kawanwa-stone",
-    storageBucket: "kawanwa-stone.appspot.com",
+    storageBucket: "kawanwa-stone.firebasestorage.app",
     messagingSenderId: "678984987842",
     appId: "1:678984987842:web:952c3f4a8822b23590f75b"
 };
+
